0bfb235ad4527aa977691638ff1539a84d20a71f
Frank Albenesius <frankalbenesius@gmail.com>
2018-04-08 23:40:57 -0500

    fix some home page wording

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'react-router-dom/Link'
 
 export default () => (
   <div>
     <h3>Welcome to Social</h3>
-    <p>You can't get social till you sign in, pal.</p>
+    <p>
+      You can't get social until you <Link to="/signin">sign in</Link>, pal.
+    </p>
     <img alt="" src="https://media.giphy.com/media/VfyC5j7sR4cso/giphy.gif" />
   </div>
 )
