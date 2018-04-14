de48618cde0a1e31c6ae5d9d37749c9f9782e050
Frank Albenesius <frankalbenesius@gmail.com>
2018-04-13 22:54:39 -0500

    gut friends concept, redoing soon

--- src/pages/Profile/Friends.js
+++ src/pages/Profile/Friends.js
@@ -1,41 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'react-router-dom/Link'
-import FriendsProvider from '../../containers/FriendsProvider.js'
-import FriendshipProvider from '../../containers/FriendshipProvider.js'
+// import Link from 'react-router-dom/Link'
 import Section from './Section.js'
 
 const Friends = ({ user, profile }) => {
-  return (
-    <Section title={'Friends'}>
-      <FriendshipProvider
-        ids={[user.id, profile.id]}
-        render={friendship => {
-          if (friendship) {
-            return (
-              <FriendsProvider
-                id={profile.id}
-                render={friends =>
-                  friends.map(friend => (
-                    <Link to={`/profile/${friend.id}`} key={friend.id}>
-                      {friend.name}
-                    </Link>
-                  ))
-                }
-              />
-            )
-          } else {
-            return (
-              <div>
-                <p>You are not friends with this user.</p>
-                <button>Send Friend Request</button>
-              </div>
-            )
-          }
-        }}
-      />
-    </Section>
-  )
+  return <Section title={'Friends'}>friends</Section>
 }
 
 const userDataType = PropTypes.shape({
